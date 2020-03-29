4c8e5865db46349435884af5bdfcd2306e0b0893
Tiet Hao Kien <timothytiet@gmail.com>
2020-03-28 21:48:21 -0700

    Split component

--- react-ui/src/app/components/content/home/index.jsx
+++ react-ui/src/app/components/content/home/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import { Activities } from '../Activies';
 import data from '../../../data/index.json';
+import { Container, Row, Col } from 'react-bootstrap';
+
 export const Home = () => {
     return (
-        <div className='container'>
-            {
-                data.map(({PHOTO, category, activity}) => 
-                    <Activities {...{ picture: PHOTO, title: activity, text: category}}/> 
-            )}
-        </div>
+        <Container fluid>
+            <Row className = "flex-xl-nowrap">
+                <Col xl ="2">1 of 3</Col>
+                <Col xl ="8">
+                    <div className = "card-wrapper">
+                    {
+                        data.map(({PHOTO, category, activity}) =>                     
+                        <Activities {...{ picture: PHOTO, title: activity, text: category}}/> 
+                    )}
+
+                    </div>
+
+                </Col>
+                <Col xl ="2">3 of 3</Col>
+            </Row>            
+        </Container>
+        // <div className='container-fluid'>
+        //     <div className = "col-ms-2">
+        //         <h1>Test1</h1>
+        //     </div>
+        //     <div className = "col-ms-8">
+        //     </div>
+        //     <div className = "col-ms-2">
+        //         <h1>Test2</h1>
+        //     </div>
+        // </div>
     ) 
 }
